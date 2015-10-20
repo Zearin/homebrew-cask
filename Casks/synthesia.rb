7f734572a481289e3ddb9f5d96ade50bf322c1ef
Zearin <Zearin@users.noreply.github.com>
2015-10-20 08:19:30 -0400

    Update synthesia.rb
    * Regarding the version:
      - At the time of this writing, The latest version is 10.2
      - I cannot find other versioned downloads on the site.  (My memory is hazy, but I think the site—and the project—have had some bigger than “version bump” changes)
    * I could not find a license to fix `license :unknown` (but it is free for partial-functionality, pay for full functionality, and also available on the Mac App Store)

--- Casks/synthesia.rb
+++ Casks/synthesia.rb
@@ -1,9 +1,8 @@
 cask :v1 => 'synthesia' do
-  version '10.1'
-  sha256 '394418e9a32cd2dc53858f8721c81832be3ca460c0cf210bd8e179f1d6b2ba1d'
+  version :latest
+  sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
-  url "https://synthesia.s3.amazonaws.com/files/Synthesia-#{version}.dmg"
+  url "http://www.synthesiagame.com/download.aspx?product=Synthesia&platform=mac&version=latest"
   name 'Synthesia'
   homepage 'http://www.synthesiagame.com'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
